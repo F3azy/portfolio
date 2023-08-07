e0633a78557c6034e5c4611c9e7d5a68b80cd579
Francesco <francescocarvelli25@gmail.com>
2023-08-07 17:16:09 +0200

    Started the tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,18 @@
+import { Flex, Text } from '@chakra-ui/react';
+import { sections } from '../constants';
+import SectionInfo from './SectionInfo';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <Flex 
+    direction="column"
+    rowGap={{base: "44px", lg: "0"}}
+    justify="space-evenly"
+    h={{lg: "100vh"}} 
+    p={{base: "44px 0", lg: "88px 0"}} 
+    >
+      <SectionInfo infos={sections.get("tech")} />
+    </Flex>
   )
 };
 
