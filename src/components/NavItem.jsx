1d48ed832e0b33ace5509f0db9553e0910440bd2
Francesco <francescocarvelli25@gmail.com>
2023-08-15 19:52:38 +0200

    adjusted the navitem so it animate the group while hovering and clicking on the circle in the desktop view doesnt do anything

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, Icon, Text, Tooltip, Link } from '@chakra-ui/react';
+import { Box, Center, Icon, Text, Tooltip, Link, textDecoration } from '@chakra-ui/react';
 import { useState, useLayoutEffect } from 'react';
 
 const NavItem = ({id, title, icon, active, setActive}) => {
@@ -23,46 +23,51 @@ const NavItem = ({id, title, icon, active, setActive}) => {
 
   return (
       <Box p={{base: "0", lg: "0 12px"}} position="relative">
-        <Center as={Link} href={`#${id}`} justifyContent={{base: "flex-start", lg: "center"}} columnGap="12px" onClick={() => setActive(title)}>
-            <Tooltip 
-            hasArrow 
-            label={`${title}`} 
-            placement='right' 
-            bg="brand.dark.900" 
-            color="brand.white"
-            m="0 0 0 16px"
-            p="4px 12px"
-            fontSize="20px"
-            borderRadius="6px"
-            isDisabled={!isLargerThan992}
-            >
-                <Center 
-                boxSize={{base: "48px", md: "60px", lg: "48px"}}
-                bg={(active==title) ? "brand.secondary" : "brand.dark.700"}
-                borderRadius={(active==title) ? "16px" : "24px"} 
-                color={(active==title) ? "brand.white" : "brand.primary"}
-                transition="border-radius 0.15s linear, background 0.2s linear, color 0.2s linear"
-                _hover={{ 
+        <Center>
+            <Center role="group" as={Link} href={`#${id}`} onClick={() => setActive(title)} w={"full"} justifyContent={{base: "flex-start", lg: "center"}} columnGap="12px">
+              <Tooltip 
+              hasArrow 
+              label={`${title}`} 
+              placement='right' 
+              bg="brand.dark.900" 
+              color="brand.white"
+              m="0 0 0 16px"
+              p="4px 12px"
+              fontSize="20px"
+              borderRadius="6px"
+              isDisabled={!isLargerThan992}
+              >
+                  <Center 
+                  boxSize={{base: "48px", md: "60px", lg: "48px"}}
+                  bg={(active==title) ? "brand.secondary" : "brand.dark.700"}
+                  borderRadius={(active==title) ? "16px" : "24px"} 
+                  color={(active==title) ? "brand.white" : "brand.primary"}
+                  transition="border-radius 0.15s linear, background 0.2s linear, color 0.2s linear"
+                  _groupHover={{ 
                     borderRadius: "16px",
                     bg: "brand.secondary",
                     color: "brand.white",
                     cursor: "pointer",
-                }}
-                onMouseEnter={()=> setIsHovering(true)} 
-                onMouseLeave={() => setIsHovering(false)}
-                >
-                    <Icon as={icon} boxSize={{base: 8, md: 10, lg: 8}}/>
-                </Center>
-            </Tooltip>
-            <Text 
-            display={{base: "block", lg: "none"}} 
-            fontSize={{base: "20px", md: "28px"}}
-            fontWeight="bold"
-            color="brand.primary"
-            decoration={(active==title) ? "underline" : "none"}
-            >
-                {title}
-            </Text>
+                  }}
+                  onMouseEnter={()=> setIsHovering(true)} 
+                  onMouseLeave={() => setIsHovering(false)}
+                  >
+                      <Icon as={icon} boxSize={{base: 8, md: 10, lg: 8}}/>
+                  </Center>
+              </Tooltip>
+              <Text 
+              display={{base: "block", lg: "none"}} 
+              fontSize={{base: "20px", md: "28px"}}
+              fontWeight="bold"
+              color="brand.primary"
+              textDecoration={(active==title) ? "underline" : "none"}
+              _groupHover={{
+                textDecoration: "underline",
+              }}
+              >
+                  {title}
+              </Text>
+            </Center>
             <Box 
             w="8px" 
             h={(active==title) ? "40px" : isHovering ? "20px" : "8px"} 
