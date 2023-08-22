58f4d101c53aa51d4ea7d54120df06b01b82d742
Francesco <francescocarvelli25@gmail.com>
2023-08-22 21:52:14 +0200

    section info animation

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -1,10 +1,15 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 const SectionInfo = ({infos}) => {
   return (
     <Box>
       <Heading 
-      as="h2" 
+      as={motion.h2} 
+      initial="hidden"
+      whileInView="visible"
+      variants={fadeIn("left", "", 0.1, 1)}
       fontSize={{base: "12px", sm: "14px", md: "16px"}} 
       color="brand.gray"
       letterSpacing="3px"
@@ -12,12 +17,19 @@ const SectionInfo = ({infos}) => {
         {infos.subTitle}
       </Heading>
       <Heading 
-      as="h1" 
+      as={motion.h1} 
+      initial="hidden"
+      whileInView="visible"
+      variants={fadeIn("left", "", 0.5, 1)}
       fontSize={{base: "36px",sm: "40px", md: "48px"}}
       >
         {infos.title}
       </Heading>
       <Text 
+      as={motion.p}
+      initial="hidden"
+      whileInView="visible"
+      variants={fadeIn("left", "", 1, 1)}
       fontSize={{base: "14px", sm: "16px", md: "18px"}} 
       lineHeight={{base: "20px", sm: "22px", md: "24px"}}
       color="brand.gray"
