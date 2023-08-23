1469bad15a928831c5673d2002020e99ce12a886
f3azy <francescocarvelli25@gmail.com>
2023-08-23 21:49:21 +0200

    aniamted the title of the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { Box, Flex, Text, Heading } from '@chakra-ui/react';
 import { HeroBG, HeroAnimation } from '../assets';
 import Lottie from 'react-lottie-player';
 import { motion } from 'framer-motion';
-import { zoomIn } from '../utils/motion';
+import { slideIn, slideOut, zoomIn } from '../utils/motion';
 
 const Hero = () => {
 
@@ -26,29 +26,72 @@ const Hero = () => {
       top={{lg: "88px"}} 
       left={{xl: "180px" ,'2xl': "320px"}}
       >
-        <Flex direction="column" align="center">
-          <Box boxSize="20px" bg="brand.tertiary" borderRadius="full" />
-          <Box w="2px" h={{base: "200px", lg: "300px"}} bg="brand.tertiary" />
+        <Flex 
+        as={motion.div} 
+        initial="hidden" 
+        animate="visible" 
+        variants={slideIn("left", "", 2, 0.5, 640)} 
+        direction="column" 
+        align="center"
+        >
+          <Box 
+          as={motion.div} 
+          initial="hidden" 
+          animate="visible" 
+          variants={slideIn("down", "", 1, 0.5, 150)} 
+          boxSize="20px" 
+          bg="brand.tertiary" 
+          borderRadius="full" 
+          />
+          <Box 
+          as={motion.div} 
+          initial="hidden" 
+          animate="visible" 
+          variants={{
+            hidden: {height: 0}, 
+            visible: {
+              height: "250px", 
+              transition: {
+                delay: 1.5,
+                duration: 0.5,
+                ease: "easeOut",
+              }
+            }}}  
+          w="2px" 
+          // h={{base: "200px", lg: "300px"}} 
+          bg="brand.tertiary" 
+          />
         </Flex>
-        <Box>
-        <Heading 
-        as="h1" 
-        fontSize={{base: "32px", md:"48px"}} 
-        lineHeight={{base: "36px", md:"40px"}}
-        letterSpacing="-0.5px"
-        >
-          Hello World, I'm <Text as="span" color="brand.tertiary">Francesco</Text>
-        </Heading>
-        <Text 
-        m={{base: "8px 0 0",md: "16px 0 0"}}
-        fontWeight="bold" 
-        fontSize={{base: "14px",sm: "20px", md: "24px"}} 
-        lineHeight={{base: "20px", sm: "26px", md:"32px"}} 
-        letterSpacing={{base: "1.5px", md: "1px"}}
-        >
-          I am a <Text as="span" color="brand.tertiary">Front-end</Text> developer, <br/>
-          aspiring to become a FullStack developer 
-        </Text>
+        <Box h="min-content" overflow="hidden" position="relative">
+          <Box 
+          as={motion.div} 
+          initial="visible" 
+          animate="hidden" 
+          variants={slideOut("left", "", 2, 0.5, 640)} 
+          bg="brand.dark.700" 
+          w="full" 
+          h="full" 
+          position="absolute" 
+          />
+          <Heading 
+          as="h1" 
+          fontSize={{base: "32px", md:"48px"}} 
+          lineHeight={{base: "36px", md:"48px"}}
+          letterSpacing="-0.5px"
+          overflow="hidden"
+          >
+            Hello World, I'm <Text as="span" color="brand.tertiary">Francesco</Text>
+          </Heading>
+          <Text 
+          m={{base: "8px 0 0",md: "16px 0 0"}}
+          fontWeight="bold" 
+          fontSize={{base: "14px",sm: "20px", md: "24px"}} 
+          lineHeight={{base: "20px", sm: "26px", md:"32px"}} 
+          letterSpacing={{base: "1.5px", md: "1px"}}
+          >
+            I am a <Text as="span" color="brand.tertiary">Front-end</Text> developer, <br/>
+            aspiring to become a FullStack developer 
+          </Text>
         </Box>
       </Flex>
       <Box 
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,8 +1,8 @@
-export const fadeIn = (direction, type, delay, duration) => {
+export const fadeIn = (direction, type, delay, duration, offset = 100) => {
     return {
         hidden: {
-            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            x: direction === "left" ? offset : direction === "right" ? -offset : 0,
+            y: direction === "up" ? offset : direction === "down" ? -offset : 0,
             opacity: 0,
             when: "afterChildren",
         },
@@ -12,6 +12,48 @@ export const fadeIn = (direction, type, delay, duration) => {
             opacity: 1,
             transition: {
                 when: "beforeChildren",
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
+
+export const slideIn = (direction, type, delay, duration, offset = 100) => {
+    return {
+        hidden: {
+            x: direction === "left" ? offset : direction === "right" ? -offset : 0,
+            y: direction === "up" ? offset : direction === "down" ? -offset : 0,
+            when: "afterChildren",
+        },
+        visible: {
+            x: 0,
+            y: 0,
+            transition: {
+                when: "beforeChildren",
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
+
+export const slideOut = (direction, type, delay, duration, offset = 100) => {
+    return {
+        visible: {
+            x: 0,
+            y: 0,
+            when: "beforeChildren",
+        },
+        hidden: {
+            x: direction === "left" ? -offset : direction === "right" ? offset : 0,
+            y: direction === "up" ? -offset : direction === "down" ? offset : 0,
+            transition: {
+                when: "afterChildren",
                 type: type,
                 delay: delay,
                 duration: duration,
@@ -32,6 +74,29 @@ export const zoomIn = (delay, duration) => {
             opacity: 1,
             transition: {
                 type: "tween",
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        }
+    };
+};
+
+export const textReveal = (direction, type, delay, duration, offset = 100) => {
+    return {
+        hidden: {
+            x: direction === "left" ? offset : direction === "right" ? -offset : 0,
+            y: direction === "up" ? offset : direction === "down" ? -offset : 0,
+            width: 0,
+            when: "afterChildren",
+        },
+        visible: {
+            x: 0,
+            y: 0,
+            width: "auto",
+            transition: {
+                when: "beforeChildren",
+                type: type,
                 delay: delay,
                 duration: duration,
                 ease: "easeOut",
