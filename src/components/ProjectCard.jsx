3f5dddf1940c206773f3337b9d2c0e092f831311
Francesco <francescocarvelli25@gmail.com>
2023-08-31 18:15:24 +0200

    added buttons to the project card for github and site

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,9 +8,14 @@ import {
     Heading,
     Box,
     Flex,
-    Icon
+    Icon,
+    Divider,
+    ButtonGroup,
+    Button,
+    CardFooter
 } from '@chakra-ui/react';
 import { BiLogoGithub } from 'react-icons/bi';
+import { RiEarthLine } from 'react-icons/ri';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../utils/motion';
 
@@ -24,31 +29,11 @@ const ProjectCard = ({title, image, githubUrl, url, description, tags}) => {
     borderRadius="16px"
     p="12px"
     height="100%"
+    rowGap="12px"
     >
       <CardBody p="0">
-        <Flex direction="column" rowGap="10px" h="full">
-            <Box position="relative">
-                <Image src={image} borderRadius="8px" />
-                <Link 
-                href={githubUrl} 
-                isExternal
-                >
-                <Icon 
-                color="#171515"
-                bg="brand.white"
-                borderRadius="full"
-                boxSize={10}
-                as={BiLogoGithub} 
-                position="absolute" 
-                top="3" 
-                right="3"
-                transition="transform 0.5s ease"
-                _hover={{
-                    transform: "scale(1.2)"
-                }} 
-                />
-                </Link>
-            </Box>
+        <Flex direction="column" rowGap="12px" h="full">
+            <Image src={image} borderRadius="8px" />
             <Heading 
             as="h3"
             color="brand.white"
@@ -66,8 +51,7 @@ const ProjectCard = ({title, image, githubUrl, url, description, tags}) => {
             >
                 {description}
             </Text>
-            <Box 
-            display="flex" 
+            <Flex 
             flexDirection="row" 
             justifyContent="flex-start"
             flexWrap="wrap"
@@ -85,9 +69,56 @@ const ProjectCard = ({title, image, githubUrl, url, description, tags}) => {
                     #{tag.name}
                 </Text>
             )}
-            </Box>
+            </Flex>
         </Flex>
       </CardBody>
+      <Divider borderColor="brand.dark.800" />
+      <CardFooter p="0">
+        <ButtonGroup spacing={{base: '4', md: '2', xl: '4'}}>
+          <Button 
+          as={Link}
+          href={url}
+          onClick={!url ? ((ev) => ev.preventDefault()) : null}
+          bg="brand.tertiary" 
+          color="brand.white" 
+          borderRadius="full" 
+          columnGap="8px"
+          _hover={{
+            bg: "brand.secondary",
+            textDecoration: "none",
+          }}
+          _active={{
+            bg: "brand.tertiary" 
+          }}
+          isExternal
+          isDisabled={!url}
+          >
+            <RiEarthLine fontSize="24px" />
+            Site
+          </Button>
+          <Button 
+          as={Link}
+          href={githubUrl}
+          onClick={!githubUrl ? ((ev) => ev.preventDefault()) : null}
+          bg="#171515" 
+          color="brand.white" 
+          borderRadius="full" 
+          columnGap="8px"
+          _hover={{
+            bg: "#060404",
+            textDecoration: "none",
+          }}
+          _active={{
+            bg: "#171515" 
+          }}
+          isExternal
+          isDisabled={!githubUrl}
+          >
+            <BiLogoGithub fontSize="24px" />
+            Github
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
     </Card>
   )
 };
