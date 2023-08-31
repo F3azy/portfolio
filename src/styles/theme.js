fd7e19aba2746823af9d3edb0119623afc785f7b
Francesco <francescocarvelli25@gmail.com>
2023-08-31 17:11:45 +0200

    chenged the font to lato

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -16,6 +16,10 @@ export const theme = extendTheme({
             },
         }
     },
+    fonts: {
+        heading: "'Lato', sans-serif",
+        body: "'Lato', sans-serif",
+    },
     styles: {
         global: () => ({
             body: {
